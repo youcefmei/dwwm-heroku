84c231aef5e1dd48011bdc6e62a6837ca49a35ed
youcef <mymail>
2022-04-19 17:07:59 +0200

    Heroku 8

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\CourseRepository;
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,13 +11,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(CourseRepository $courseRepository): Response
+    public function index(CourseRepository $courseRepository,UserRepository $userRepository): Response
     {
-        $courses = $courseRepository->findBy(["is_published"=>true],$orderBy=['published_at'=>'desc'],limit:3);
-        return $this->render('home/index.html.twig', [
-            'courses' => $courses,
-            
-        ]);
+        $users = $userRepository->findAll();
+        if ($users){
+
+            $courses = $courseRepository->findBy(["is_published"=>true],$orderBy=['published_at'=>'desc'],limit:3);
+            return $this->render('home/index.html.twig', [
+                'courses' => $courses,
+                
+            ]);
+        }
+        else{
+            $this->redirectToRoute('admin.register');
+        }
     }
 
 
