347d816c6f39952aeefa9b1db68445f880685c86
youcef <mymail>
2022-04-19 16:51:52 +0200

    Heroku 7

---
+++ migrations/Version20220419145114.php
@@ -0,0 +1,87 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220419145114 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE admin (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, firstname VARCHAR(190) NOT NULL, lastname VARCHAR(190) NOT NULL, UNIQUE INDEX UNIQ_880E0D76A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE answer_wrong (id INT AUTO_INCREMENT NOT NULL, question_id INT NOT NULL, title VARCHAR(190) NOT NULL, INDEX IDX_BCFF74491E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE course (id INT AUTO_INCREMENT NOT NULL, teacher_id INT DEFAULT NULL, title VARCHAR(190) NOT NULL, slug VARCHAR(190) NOT NULL, description LONGTEXT NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', is_published TINYINT(1) NOT NULL, image VARCHAR(190) NOT NULL, updated_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', published_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', image_size INT NOT NULL, UNIQUE INDEX UNIQ_169E6FB92B36786B (title), UNIQUE INDEX UNIQ_169E6FB9989D9B62 (slug), INDEX IDX_169E6FB941807E1D (teacher_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE course_student (id INT AUTO_INCREMENT NOT NULL, course_id INT NOT NULL, student_id INT NOT NULL, joined_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', progress SMALLINT NOT NULL, INDEX IDX_BFE0AADF591CC992 (course_id), INDEX IDX_BFE0AADFCB944F1A (student_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE lesson (id INT AUTO_INCREMENT NOT NULL, section_id INT DEFAULT NULL, teacher_id INT NOT NULL, content LONGTEXT NOT NULL, slug VARCHAR(190) NOT NULL, update_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', is_published TINYINT(1) DEFAULT 1 NOT NULL, title VARCHAR(190) NOT NULL, media VARCHAR(190) NOT NULL, INDEX IDX_F87474F3D823E37A (section_id), INDEX IDX_F87474F341807E1D (teacher_id), UNIQUE INDEX section_title_idx (title, section_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE lesson_student (id INT AUTO_INCREMENT NOT NULL, lesson_id INT NOT NULL, student_id INT NOT NULL, INDEX IDX_425FFD94CDF80196 (lesson_id), INDEX IDX_425FFD94CB944F1A (student_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, teacher_id INT NOT NULL, quiz_id INT DEFAULT NULL, title VARCHAR(190) NOT NULL, correct_response VARCHAR(190) NOT NULL, INDEX IDX_B6F7494E41807E1D (teacher_id), INDEX IDX_B6F7494E853CD175 (quiz_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE quiz (id INT AUTO_INCREMENT NOT NULL, section_id INT NOT NULL, teacher_id INT NOT NULL, title VARCHAR(190) NOT NULL, UNIQUE INDEX UNIQ_A412FA92D823E37A (section_id), INDEX IDX_A412FA9241807E1D (teacher_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE section (id INT AUTO_INCREMENT NOT NULL, course_id INT NOT NULL, teacher_id INT NOT NULL, title VARCHAR(190) NOT NULL, slug VARCHAR(190) NOT NULL, added_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', is_published TINYINT(1) NOT NULL, INDEX IDX_2D737AEF591CC992 (course_id), INDEX IDX_2D737AEF41807E1D (teacher_id), UNIQUE INDEX course_title_idx (title, course_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE student (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, username VARCHAR(190) NOT NULL, UNIQUE INDEX UNIQ_B723AF33A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE teacher (id INT AUTO_INCREMENT NOT NULL, user_id INT NOT NULL, firstname VARCHAR(190) NOT NULL, lastname VARCHAR(190) NOT NULL, description LONGTEXT NOT NULL, approved_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', image VARCHAR(190) NOT NULL, image_size INT NOT NULL, updated_at DATETIME NOT NULL, is_approved TINYINT(1) NOT NULL, UNIQUE INDEX UNIQ_B0F6A6D5A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, email VARCHAR(190) NOT NULL, roles LONGTEXT NOT NULL COMMENT \'(DC2Type:json)\', password VARCHAR(255) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE admin ADD CONSTRAINT FK_880E0D76A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE answer_wrong ADD CONSTRAINT FK_BCFF74491E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
+        $this->addSql('ALTER TABLE course ADD CONSTRAINT FK_169E6FB941807E1D FOREIGN KEY (teacher_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE course_student ADD CONSTRAINT FK_BFE0AADF591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE course_student ADD CONSTRAINT FK_BFE0AADFCB944F1A FOREIGN KEY (student_id) REFERENCES student (id)');
+        $this->addSql('ALTER TABLE lesson ADD CONSTRAINT FK_F87474F3D823E37A FOREIGN KEY (section_id) REFERENCES section (id)');
+        $this->addSql('ALTER TABLE lesson ADD CONSTRAINT FK_F87474F341807E1D FOREIGN KEY (teacher_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE lesson_student ADD CONSTRAINT FK_425FFD94CDF80196 FOREIGN KEY (lesson_id) REFERENCES lesson (id)');
+        $this->addSql('ALTER TABLE lesson_student ADD CONSTRAINT FK_425FFD94CB944F1A FOREIGN KEY (student_id) REFERENCES student (id)');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E41807E1D FOREIGN KEY (teacher_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id)');
+        $this->addSql('ALTER TABLE quiz ADD CONSTRAINT FK_A412FA92D823E37A FOREIGN KEY (section_id) REFERENCES section (id)');
+        $this->addSql('ALTER TABLE quiz ADD CONSTRAINT FK_A412FA9241807E1D FOREIGN KEY (teacher_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE section ADD CONSTRAINT FK_2D737AEF591CC992 FOREIGN KEY (course_id) REFERENCES course (id)');
+        $this->addSql('ALTER TABLE section ADD CONSTRAINT FK_2D737AEF41807E1D FOREIGN KEY (teacher_id) REFERENCES teacher (id)');
+        $this->addSql('ALTER TABLE student ADD CONSTRAINT FK_B723AF33A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE teacher ADD CONSTRAINT FK_B0F6A6D5A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE course_student DROP FOREIGN KEY FK_BFE0AADF591CC992');
+        $this->addSql('ALTER TABLE section DROP FOREIGN KEY FK_2D737AEF591CC992');
+        $this->addSql('ALTER TABLE lesson_student DROP FOREIGN KEY FK_425FFD94CDF80196');
+        $this->addSql('ALTER TABLE answer_wrong DROP FOREIGN KEY FK_BCFF74491E27F6BF');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E853CD175');
+        $this->addSql('ALTER TABLE lesson DROP FOREIGN KEY FK_F87474F3D823E37A');
+        $this->addSql('ALTER TABLE quiz DROP FOREIGN KEY FK_A412FA92D823E37A');
+        $this->addSql('ALTER TABLE course_student DROP FOREIGN KEY FK_BFE0AADFCB944F1A');
+        $this->addSql('ALTER TABLE lesson_student DROP FOREIGN KEY FK_425FFD94CB944F1A');
+        $this->addSql('ALTER TABLE course DROP FOREIGN KEY FK_169E6FB941807E1D');
+        $this->addSql('ALTER TABLE lesson DROP FOREIGN KEY FK_F87474F341807E1D');
+        $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E41807E1D');
+        $this->addSql('ALTER TABLE quiz DROP FOREIGN KEY FK_A412FA9241807E1D');
+        $this->addSql('ALTER TABLE section DROP FOREIGN KEY FK_2D737AEF41807E1D');
+        $this->addSql('ALTER TABLE admin DROP FOREIGN KEY FK_880E0D76A76ED395');
+        $this->addSql('ALTER TABLE student DROP FOREIGN KEY FK_B723AF33A76ED395');
+        $this->addSql('ALTER TABLE teacher DROP FOREIGN KEY FK_B0F6A6D5A76ED395');
+        $this->addSql('DROP TABLE admin');
+        $this->addSql('DROP TABLE answer_wrong');
+        $this->addSql('DROP TABLE course');
+        $this->addSql('DROP TABLE course_student');
+        $this->addSql('DROP TABLE lesson');
+        $this->addSql('DROP TABLE lesson_student');
+        $this->addSql('DROP TABLE question');
+        $this->addSql('DROP TABLE quiz');
+        $this->addSql('DROP TABLE section');
+        $this->addSql('DROP TABLE student');
+        $this->addSql('DROP TABLE teacher');
+        $this->addSql('DROP TABLE user');
+    }
+}
