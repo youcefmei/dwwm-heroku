9117e4d919264f5ce2ef7938418035cdf9471195
youcef <mymail>
2022-04-20 12:02:52 +0200

    bug homecontroller solved

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -13,14 +13,13 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(CourseRepository $courseRepository,UserRepository $userRepository): Response
     {
-        return $this->redirectToRoute('admin.register');
         $users = $userRepository->findAll();
         if ($users){
 
             $courses = $courseRepository->findBy(["is_published"=>true],$orderBy=['published_at'=>'desc'],limit:3);
             return $this->render('home/index.html.twig', [
                 'courses' => $courses,
-                
+
             ]);
         }
         else{
