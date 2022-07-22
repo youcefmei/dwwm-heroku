5eb3eeb5e23e45092f86c28eb38fdbef4c8b0f30
youcefmei <youcefmei@aol.com>
2022-07-22 10:07:34 +0200

    teacher fix don't try show progress

--- public/js/lesson.js
+++ public/js/lesson.js
@@ -1,9 +1,11 @@
 let checkComplete = document.getElementById("lesson-complete")
-checkComplete.addEventListener("change", (e) => {
-  var previousUrl = window.location.href
-  var url = new URL(previousUrl)
-  const urlSearchParams = new URLSearchParams(window.location.search)
-  urlSearchParams.set(checkComplete.name, checkComplete.value)
-  url.search = urlSearchParams.toString()
-  location.replace(url)
-})
+if (checkComplete) {
+  checkComplete.addEventListener("change", (e) => {
+    var previousUrl = window.location.href
+    var url = new URL(previousUrl)
+    const urlSearchParams = new URLSearchParams(window.location.search)
+    urlSearchParams.set(checkComplete.name, checkComplete.value)
+    url.search = urlSearchParams.toString()
+    location.replace(url)
+  })
+}
